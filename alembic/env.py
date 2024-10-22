3756dfb87823d3f045a9f0676c67d384cb2a3666
Spaceocean <spaceocean@outlook.com>
2024-10-22 10:27:43 +0500

    added config for alembic

--- alembic/env.py
+++ alembic/env.py
@@ -2,28 +2,30 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-
+from app.models import Base
 from alembic import context
 
-# this is the Alembic Config object, which provides
-# access to the values within the .ini file in use.
+import os
+from dotenv import load_dotenv
+
 config = context.config
 
-# Interpret the config file for Python logging.
-# This line sets up loggers basically.
+# Загрузка переменных окружения из файла .env
+load_dotenv()
+
+# Получение данных из переменных окружения
+username = os.getenv("DB_USERNAME")
+password = os.getenv("DB_PASSWORD")
+host = os.getenv("DB_HOST")
+dbname = os.getenv("DB_NAME")
+
+# Формирование строки подключения
+config.set_main_option('sqlalchemy.url', f'postgresql://{username}:{password}@{host}/{dbname}')
+
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-# add your model's MetaData object here
-# for 'autogenerate' support
-# from myapp import mymodel
-# target_metadata = mymodel.Base.metadata
-target_metadata = None
-
-# other values from the config, defined by the needs of env.py,
-# can be acquired:
-# my_important_option = config.get_main_option("my_important_option")
-# ... etc.
+target_metadata = Base.metadata
 
 
 def run_migrations_offline() -> None:
