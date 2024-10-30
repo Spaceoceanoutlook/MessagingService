98ece776c363a18c5882dc7bef75b07b08aa7103
Spaceocean <spaceocean@outlook.com>
2024-10-30 16:37:21 +0500

    edit jwt.py

--- messagingservice/api_v1/auth/jwt.py
+++ messagingservice/api_v1/auth/jwt.py
@@ -6,27 +6,24 @@ from fastapi.responses import RedirectResponse
 
 SECRET_KEY = secrets.token_urlsafe(32)
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
+ACCESS_TOKEN_EXPIRE_MINUTES = 10
 
 
-def create_access_token(data: dict, expires_delta: timedelta = None):
+def create_access_token(data: dict):
     to_encode = data.copy()
-    expire = (datetime.now(timezone.utc) +
-              (expires_delta if expires_delta else timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)))
+    expire = datetime.now(timezone.utc) + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
 
 def set_access_token(response: RedirectResponse, username: str):
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(data={"username": username}, expires_delta=access_token_expires)
+    access_token = create_access_token(data={"username": username})
     response.set_cookie(key="access_token", value=access_token, httponly=True)
     return response
 
 
 def verify_token(access_token: str):
     try:
-        payload = jwt.decode(access_token, SECRET_KEY, algorithms=[ALGORITHM])
-        return payload
+        return jwt.decode(access_token, SECRET_KEY, algorithms=[ALGORITHM])
     except JWTError:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid token")
