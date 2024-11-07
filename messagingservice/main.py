bee89c5d11aaced4340a1cf5a384f1beb9142893
Spaceocean <spaceocean@outlook.com>
2024-11-07 12:47:17 +0500

    rename chat to profile

--- messagingservice/main.py
+++ messagingservice/main.py
@@ -1,7 +1,7 @@
 import uvicorn
 from fastapi import FastAPI
 from messagingservice.api_v1.auth.crud import router as auth_router
-from messagingservice.api_v1.chat.crud import router as profile_router
+from messagingservice.api_v1.profile.crud import router as profile_router
 
 app = FastAPI()
 app.include_router(auth_router)
